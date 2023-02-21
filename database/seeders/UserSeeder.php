3ccbfa2d3d066a269e9837286110e31a44c999cb
zabiremu <zabirraihan@gmail.com>
2023-02-21 13:50:32 +0600

    role and permission bug fix

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,11 +16,19 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+
+        $user = new User();
+        $user->name = 'admin';
+        $user->email = 'admin@admin.com';
+        $user->password = Hash::make('password');
+        $user->save();
+        $user->assignRole('admin');;
+
         $user = new User();
         $user->name = 'Md Parvej';
         $user->email = 'pj.parvaz45@gmail.com';
         $user->password = Hash::make('12345678');
         $user->save();
-
+        $user->assignRole('buyer');
     }
 }
